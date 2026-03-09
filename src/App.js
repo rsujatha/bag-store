c8f1c73a46481c1bd6fe3b3ff9e31b07c6234095
Sujatha Ramakrishnan <sujatha.ramakrishnan.7@gmail.com>
2026-03-09 18:18:29 +0530

    Refactor image rendering in App.js

--- src/App.js
+++ src/App.js
@@ -88,10 +88,19 @@ function ProductCard({ product, allImages, onNavigate }) {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      {allImages.length > 0
-        ? <img src={allImages[imgIndex]} alt={product.product_name} />
-        : <PlaceholderImage name={product.product_name} />
-      }
+      <div className="card-image-stack">
+        {allImages.length > 0
+          ? allImages.map((src, i) => (
+              <img
+                key={src}
+                src={src}
+                alt={product.product_name}
+                className={`stack-img ${i === imgIndex ? 'visible' : ''}`}
+              />
+            ))
+          : <PlaceholderImage name={product.product_name} />
+        }
+      </div>
       {allImages.length > 1 && (
         <div className="image-dots">
           {allImages.map((_, i) => (
