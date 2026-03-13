200e22b0c7c50589ffe98486304a05c881740ff5
Sujatha Ramakrishnan <sujatha.ramakrishnan.7@gmail.com>
2026-03-13 20:56:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { CartProvider } from './CartContext';
+import { WishlistProvider } from './WishlistContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CartProvider>
-      <App />
+      <WishlistProvider>
+        <App />
+      </WishlistProvider>
     </CartProvider>
   </React.StrictMode>
 );
